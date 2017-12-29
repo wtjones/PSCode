968df1460d93a29165cb2c19f3daa9c0a6b90633
William T Jones <wtravisjones@gmail.com>
2017-12-29 16:37:34 -0600

    Resove #4 allow additional args to pass to code.exe

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,8 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 
 ## [Unreleased]
+### Changed
+- Resove [#4](https://github.com/wtjones/PSCode/issues/4) allow additional args to pass to code.exe
 
 ## [1.0.2] - 2017-5-15
 ### Added
--- functions/Invoke-VSCode.ps1
+++ functions/Invoke-VSCode.ps1
@@ -3,17 +3,28 @@
 #>
 function Invoke-VSCode() {
     [CmdletBinding()]
-    param ($args)
+    param (        
+        [Parameter(         
+            mandatory=$false,               
+            Position=0,     
+            ValueFromRemainingArguments=$true)]
+        [String[]]$allArgs
+    )
     PROCESS {
+                    
+        $codePath = join-path $script:vscodePath 'code.exe'
+        $resourcePath = join-path $script:vscodePath 'resources\app\out\cli.js'
+
+        $codeArgs = @(('"{0}"' -f $resourcePath)) + $allArgs | select -uniq
+        
+        Write-Verbose ($codeArgs | convertTo-json)
+              
         $prior1 = $env:ELECTRON_RUN_AS_NODE
         $prior2 = $env:VSCODE_DEV 
         $env:ELECTRON_RUN_AS_NODE=1
         $env:VSCODE_DEV = $null
 
-        $codePath = join-path $script:vscodePath 'code.exe'
-        $resourcePath = join-path $script:vscodePath 'resources\app\out\cli.js'
-
-        Start-Process $codePath ('"{0}" "{1}"' -f $resourcePath, $args)
+        Start-Process -FilePath $codePath -ArgumentList $codeArgs
 
         $env:ELECTRON_RUN_AS_NODE = $prior1
         $env:VSCODE_DEV = $prior2
