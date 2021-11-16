ab5c8eb35fcb29cfa772c662b6acd659333cb902
William T Jones <wtravisjones@gmail.com>
2021-11-15 18:13:36 -0800

    Resolve #12 pass new arg due to VS Code change

--- functions/Invoke-VSCode.ps1
+++ functions/Invoke-VSCode.ps1
@@ -19,7 +19,7 @@ function Invoke-VSCode() {
 
         $codePath = join-path $script:vscodePath 'code.exe'
         $resourcePath = join-path $script:vscodePath 'resources\app\out\cli.js'
-
+        $allArgs = $allArgs + "--ms-enable-electron-run-as-node"
         $codeArgs = GetArgs $resourcePath $allArgs
         Write-Verbose ("Launching VSCode with args:`n{0}" -f ($codeArgs | convertTo-json))
     }
